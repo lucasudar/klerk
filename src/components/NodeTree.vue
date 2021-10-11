e6f44023514ef1941ca31ac94b0045721b081eb8
_Lucas_ <lucasudar@gmail.com>
2021-10-11 21:38:55 +0300

    calculated count for labels

--- src/components/NodeTree.vue
+++ src/components/NodeTree.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="node-tree">
-    <span class="label">{{ node.title }}</span>
+    <span class="label">{{ node.title }} ({{ node.count }} {{ sumOfChildren }})</span>
     <ul v-if="node.children && node.children.length">
       <node v-for="child in node.children" :node="child" :key="child.index"></node>
     </ul>
@@ -12,6 +12,19 @@ export default {
   name: 'node',
   props: {
     node: Object
+  },
+  computed: {
+    sumOfChildren () {
+      if (Array.isArray(this.node.children)) {
+        return this.node.children.reduce((prev, current) => prev + current.count, 0)
+      } else {
+        let sum = 0
+        for (const subnode of Object.values(this.node)) {
+          sum += this.sumOfChildren(subnode)
+        }
+        return sum
+      }
+    }
   }
 }
 </script>
